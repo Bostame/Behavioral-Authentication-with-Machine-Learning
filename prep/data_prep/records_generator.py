2ec2b261fc4eace5fd1500fb1a9d11152c9c4ea7
Md Shafiul Haque <md.haque@uni-potsdam.de>
2019-07-22 09:57:19 +0200

    add(data_prep): Add seventy-thirty split into training and test data
    - per person we have ten class/activity and divide each activity as seventy/thirty split

--- prep/data_prep/records_generator.py
+++ prep/data_prep/records_generator.py
@@ -1,10 +1,12 @@
-from utils import util
+import glob
+import math
 import os
 import sys
-import glob
+
 import tensorflow as tf
 from tqdm import tqdm
 
+from utils import util
 from utils.read_config import ReadConfig
 
 
@@ -13,6 +15,7 @@ def tfrecord_debugger(fn):
     :param fn: tf record
     :return: printing of shape of the tf record file elements
     """
+
     def tfrec_data_input_fn(filenames, batch_size=64, shuffle=False):
         def _input_fn():
             def _parse_record(tf_record):
@@ -35,6 +38,7 @@ def tfrecord_debugger(fn):
                 label = tf.one_hot(label, depth=18)
 
                 return image_raw, label
+
             dataset = tf.data.TFRecordDataset(filenames)
             dataset = dataset.map(_parse_record)
             if shuffle:
@@ -47,13 +51,16 @@ def tfrecord_debugger(fn):
             # y = labels
             # return X, y
             return features, labels
+
         return _input_fn
+
     #
     tfrec_dev_input_fn = tfrec_data_input_fn([fn], batch_size=64)
     features, labels = tfrec_dev_input_fn()
     with tf.Session() as sess:
         img, label = sess.run([features, labels])
-        print("are these ones the correct dimensions? {}-{}".format(img.shape, label.shape))
+        print("are these ones the correct dimensions? {}-{}".format(img.shape,
+                                                                    label.shape))
     sess.close()
     return None
 
@@ -148,7 +155,8 @@ def _process_examples(example_data, filename: str, channels=1):
     dataset_length = len(example_data)
     with tf.python_io.TFRecordWriter(filename) as writer:
         for index, ex in enumerate(example_data):
-            sys.stdout.write(f"\rProcessing sample {index + 1} of {dataset_length}")
+            sys.stdout.write(
+                f"\rProcessing sample {index + 1} of {dataset_length}")
             sys.stdout.flush()
             image_raw = ex['image'].flatten()
             image_raw = image_raw.tostring()
@@ -156,7 +164,8 @@ def _process_examples(example_data, filename: str, channels=1):
                 'height': _int64_feature(ex['image'].shape[0]),
                 'width': _int64_feature(ex['image'].shape[1]),
                 'depth': _int64_feature(channels),
-                'label': _int64_feature(int(_10_class_encoding(ex['label']))), # _encode_labels
+                'label': _int64_feature(int(_10_class_encoding(ex['label']))),
+                # _encode_labels
                 'image_raw': _bytes_feature(image_raw)
             }))
             writer.write(example.SerializeToString())
@@ -176,7 +185,8 @@ def stack_data_chunks(chunk):
     return full_data
 
 
-def transform_preprocess_data(path, out_dir, name: str, channels=None, test_persons=None, eval_persons=None,
+def transform_preprocess_data(path, out_dir, name: str, channels=None,
+                              test_persons=None, eval_persons=None,
                               chunk_len=50, num_tfrecords=10):
     """
     :param path: path of the trackings
@@ -198,22 +208,30 @@ def transform_preprocess_data(path, out_dir, name: str, channels=None, test_pers
     filenames = glob.glob('{}/*'.format(path))
     data_files = list()
     if name is 'test' and test_persons is not None:
-        data_files = [fn for fn in filenames if os.path.basename(fn).split('_')[0] in test_persons]
+        data_files = [fn for fn in filenames if
+                      os.path.basename(fn).split('_')[0] in test_persons]
     if name is 'evaluation' and eval_persons is not None:
-        data_files = [fn for fn in filenames if os.path.basename(fn).split('_')[0] in eval_persons]
+        data_files = [fn for fn in filenames if
+                      os.path.basename(fn).split('_')[0] in eval_persons]
     if name is 'train':
-        data_files = [fn for fn in filenames if os.path.basename(fn).split('_')[0] not in [test_persons, eval_persons]]
+        data_files = [fn for fn in filenames if
+                      os.path.basename(fn).split('_')[0] not in [test_persons,
+                                                                 eval_persons]]
     #
-    chunks = [data_files[k:k + chunk_len] for k in range(0, len(data_files), chunk_len)]
+    chunks = [data_files[k:k + chunk_len] for k in
+              range(0, len(data_files), chunk_len)]
     # len_chunks = len(chunks)
     for idx, c in enumerate(tqdm(chunks)):
         chunk_data = stack_data_chunks(c)
         samples_per_tf_record = len(chunk_data) // num_tfrecords
-        tf_parts = [(k * samples_per_tf_record) for k in range(len(chunk_data)) if (k * samples_per_tf_record) < len(chunk_data)]
+        tf_parts = [(k * samples_per_tf_record) for k in range(len(chunk_data))
+                    if (k * samples_per_tf_record) < len(chunk_data)]
         for i, j in enumerate(tf_parts):
-            out_fn = os.path.join(out_dir, '{}_{:03d}_{:03d}-{:03d}.tfrecord'.format(name, i+1, idx+1, num_tfrecords))
+            out_fn = os.path.join(out_dir,
+                                  '{}_{:03d}_{:03d}-{:03d}.tfrecord'.format(
+                                      name, i + 1, idx + 1, num_tfrecords))
             _process_examples(chunk_data[j:(j + samples_per_tf_record)],
-                              out_fn,channels=channels)
+                              out_fn, channels=channels)
     return None
 
 
@@ -227,7 +245,8 @@ def generate_records():
     test = ['d847614e-4', '2f3d5c91-2']
     transform_preprocess_data(trackings, out_dir, 'train')
     transform_preprocess_data(trackings, out_dir, 'test', test_persons=test)
-    transform_preprocess_data(trackings, out_dir, 'evaluation', eval_persons=evaluation)
+    transform_preprocess_data(trackings, out_dir, 'evaluation',
+                              eval_persons=evaluation)
     return None
 
 
@@ -243,9 +262,13 @@ def _20190710_generate_records():
     util.mdir(out_dir)
     evaluation = ['0fc8854f-c', '27089a2b-2', '0b7cf78c-1']
     test = ['d847614e-4', '2f3d5c91-2']
-    transform_preprocess_data(trackings, out_dir, 'train', channels=3, num_tfrecords=5)
-    transform_preprocess_data(trackings, out_dir, 'test', test_persons=test, channels=3, num_tfrecords=5)
-    transform_preprocess_data(trackings, out_dir, 'evaluation', eval_persons=evaluation, channels=3, num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'train', channels=3,
+                              num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'test', test_persons=test,
+                              channels=3, num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'evaluation',
+                              eval_persons=evaluation, channels=3,
+                              num_tfrecords=5)
     return None
 
 
@@ -262,10 +285,123 @@ def _20190712_generate_records_10classes_acgy():
     util.mdir(out_dir)
     evaluation = ['0fc8854f-c', '27089a2b-2', '0b7cf78c-1']
     test = ['d847614e-4', '2f3d5c91-2']
-    transform_preprocess_data(trackings, out_dir, 'train', channels=3, num_tfrecords=5)
-    transform_preprocess_data(trackings, out_dir, 'test', test_persons=test, channels=3, num_tfrecords=5)
-    transform_preprocess_data(trackings, out_dir, 'evaluation', eval_persons=evaluation, channels=3, num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'train', channels=3,
+                              num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'test', test_persons=test,
+                              channels=3, num_tfrecords=5)
+    transform_preprocess_data(trackings, out_dir, 'evaluation',
+                              eval_persons=evaluation, channels=3,
+                              num_tfrecords=5)
     return None
 
+
+def train_test_split_preprocess_data(path, out_dir, name: str = None,
+                                     channels=None, eval_persons=None,
+                                     chunk_len=50, num_tfrecords=10):
+    """
+    :param path: path of the trackings
+    :param out_dir: path to save the tf records
+    :param name: name of the records
+    :param channels: number of channels None means 1 by default
+    :param test_persons: Persons that are going to be included in test/validation
+    :param eval_persons: Persons that are going to be included in evaluation
+    :param chunk_len: Number of compressed files for one chunk data
+    :param num_tfrecords: Number of records in which the chunk data is going to be saved
+    :return: None
+    """
+    if str(path).endswith('/'):
+        path = path[:-1]
+    #
+    if channels is None:
+        channels = 1
+    #
+    filenames = glob.glob('{}/*'.format(path))
+    data_files = list()
+    if name is 'evaluation' and eval_persons is not None:
+        data_files = [fn for fn in filenames if
+                      os.path.basename(fn).split('_')[0] in eval_persons]
+        generate_examples(None, data_files, out_dir, channels, name, chunk_len,
+                          num_tfrecords)
+    else:
+        data_files = [fn for fn in filenames if
+                      os.path.basename(fn).split('_')[0] not in eval_persons]
+
+        for activity in important_labels:
+            activity_wise_data_files = [i for i in data_files if activity in i]
+
+            seventy_percent = math.ceil(len(activity_wise_data_files) * 0.7)
+
+            generate_examples(activity, activity_wise_data_files[
+                                        :seventy_percent],
+                              out_dir, channels, 'train', chunk_len,
+                              num_tfrecords)
+            generate_examples(activity, activity_wise_data_files[
+                                        seventy_percent:],
+                              out_dir, channels, 'test', chunk_len,
+                              num_tfrecords)
+
+    return None
+
+
+def generate_examples(activity, data_files, out_dir, channels, name,
+                      chunk_len=50,
+                      num_tfrecords=5):
+    chunks = [data_files[k:k + chunk_len] for k in
+              range(0, len(data_files), chunk_len)]
+    for idx, c in enumerate(tqdm(chunks)):
+        chunk_data = stack_data_chunks(c)
+        samples_per_tf_record = len(chunk_data) // num_tfrecords
+        tf_parts = [(k * samples_per_tf_record) for k in range(len(chunk_data))
+                    if (k * samples_per_tf_record) < len(chunk_data)]
+        for i, j in enumerate(tf_parts):
+            if activity is None:
+                file_name = '{}_{:03d}_{:03d}-{:03d}.tfrecord'.format(
+                    name, i + 1, idx + 1, num_tfrecords)
+            else:
+                file_name = '{}_{:03d}_{:03d}_{}-{:03d}.tfrecord'.format(
+                    name, i + 1, idx + 1, activity,
+                    num_tfrecords)
+            out_fn = os.path.join(out_dir, file_name)
+            _process_examples(chunk_data[j:(j + samples_per_tf_record)],
+                              out_fn, channels=channels)
+
+
+def _20190712_generate_records_10classes_train_test_split():
+    """
+    # ACCE AND GYRO DATA
+    :return: REMEMBER TO ELIMINATE FROM THE FOLDER OF THE RECORDS, THE FILES WITH SMALL SIZE, THEY ARE NOT USEFUL
+    ### THEY HAVE FEW EXAMPLES
+    """
+    read_config = ReadConfig()
+    file_path = read_config.parse_file_path()
+    trackings = file_path.spectrogram_dir
+    out_dir = file_path.training_dir
+    util.mdir(out_dir)
+    evaluation = ['0fc8854f-c', '27089a2b-2', '0b7cf78c-1']
+    test = ['d847614e-4', '2f3d5c91-2']
+    # train_test_split_preprocess_data(trackings, out_dir, '', )
+    train_test_split_preprocess_data(trackings, out_dir,
+                                     eval_persons=evaluation,
+                                     channels=3, num_tfrecords=5)
+    train_test_split_preprocess_data(trackings, out_dir,name='evaluation',
+                                     eval_persons=evaluation,
+                                     channels=3, num_tfrecords=5)
+    return None
+
+
+important_labels = [
+    'backpack_activity_step',
+    'holding_right_hand_activity_step',
+    'jacket_outer_left_pocket_activity_step',
+    'landscape_texting_activity_step',
+    'listening_voice_message_activity_step',
+    'reading_scrolling_activity_step',
+    'reading_watching_activity_step',
+    'recording_voice_message_activity_step',
+    'telephoning_activity_step',
+    'texting_activity_step'
+]
+
 if __name__ == '__main__':
-    _20190712_generate_records_10classes_acgy()
+    # _20190712_generate_records_10classes_acgy()
+    _20190712_generate_records_10classes_train_test_split()
